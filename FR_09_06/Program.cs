f3871f0f2b3ef051c5858cf6a94146c86e22d061
m.dabrowski <m.dabrowski@acars.com.pl>
2023-06-16 13:23:17 +0200

    Finished task FR_09_06. All tests passed.

---
+++ FR_09_06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+//https://pl.spoj.com/problems/PA05_POT/
+namespace FR_09_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterations = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < iterations; i++)
+            {
+                string input = Console.ReadLine();
+                char[] chars = input.ToCharArray();
+
+                var minValue = chars.Min(x => x);
+                var maxValue = chars.Max(x => x);
+
+                Console.WriteLine(maxValue - minValue);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
