d7cc01859134a3acde623ef6ee0fa2007cd0c4c8
m.dabrowski <m.dabrowski@acars.com.pl>
2023-06-16 10:05:14 +0200

    Finished task POTSAM. Tests passed.

---
+++ POTSAM/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//https://pl.spoj.com/problems/POTSAM/
+namespace spoje_praca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine().Split();
+            var n1 = int.Parse(s[0]);
+            var k1 = int.Parse(s[1]);
+            var n2 = int.Parse(s[2]);
+            var k2 = int.Parse(s[3]);
+
+            Console.WriteLine(n1 * k1 + n2 * k2);
+            Console.ReadKey();
+        }
+    }
+}
