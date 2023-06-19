f8578cc88e5a5fc6c82fae40233967e8a5ea157a
m.dabrowski <m.dabrowski@acars.com.pl>
2023-06-19 13:39:40 +0200

    Added KC001 SPOJ.

---
+++ FLAMASTE/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//https://pl.spoj.com/problems/KC001/
+namespace spoje_praca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba = Convert.ToInt16(Console.ReadLine());
+            int liczba1 = Convert.ToInt16(Console.ReadLine());
+            int liczba2 = Convert.ToInt16(Console.ReadLine());
+
+
+            Console.WriteLine(liczba + liczba1 + liczba2);
+            Console.ReadKey();
+        }
+    }
+}
