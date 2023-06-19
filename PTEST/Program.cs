52c983534f2d66fe526c3906c3f6be53801e0234
m.dabrowski <m.dabrowski@acars.com.pl>
2023-06-19 15:42:54 +0200

    Added FR_16_03

---
+++ PTEST/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt16(Console.ReadLine());
+            int b = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
